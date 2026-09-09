b91392562bcaaae02ff4063a920440d0b2d9ff77
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2026-09-09 20:16:47 -0300

    criacao das functions filtrar por busca,prioridade e ordenar por prazo

--- app.js
+++ app.js
@@ -29,13 +29,3 @@ async function iniciar() {
 }
 
 iniciar();
-
-const estado = {
-  tarefas: [],
-  busca: "",
-  status: "todos",
-  prioridade: "todas",
-  ordenacao: "crescente",
-  carregando: true,
-  erro: null,
-};
--- derivacao.js
+++ derivacao.js
@@ -7,3 +7,16 @@ const estado = {
   carregando: true,
   erro: null,
 };
+
+export  function filtrarPorBusca(tarefas,busca){
+ return tarefas.filter((item) => item.titulo.toLowerCase().includes(busca.toLowerCase()))
+}
+
+export function filtrarPorStatus(tarefas,status){
+    return tarefas.filter((item) => item.status.toLowerCase()===status.toLowerCase()||status.toLowerCase()==="todos")
+
+}
+
+export function filtrarPorPrioridade(tarefas,prioridade){
+    return tarefas.filter((item)=>item.prioridade.toLowerCase()===prioridade.toLowerCase()||prioridade.toLowerCase()==="todas")
+}
