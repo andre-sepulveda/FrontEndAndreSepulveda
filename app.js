057742eab2d68b570a12be8979e62c243934bd8d
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2026-09-09 19:35:31 -0300

    criacao da const estado no arquivo app.js

--- app.js
+++ app.js
@@ -29,3 +29,13 @@ async function iniciar() {
 }
 
 iniciar();
+
+const estado = {
+  tarefas: [],
+  busca: "",
+  status: "todos",
+  prioridade: "todas",
+  ordenacao: "crescente",
+  carregando: true,
+  erro: null,
+};
